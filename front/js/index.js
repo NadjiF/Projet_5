21ea9f9a9a1a49c2e1c840b7cc4848c5b60713f1
Nadji Fali <fali.nadji@hotmail.fr>
2022-01-06 17:39:33 +0100

    modif js index

--- front/js/index.js
+++ front/js/index.js
@@ -1,11 +1,31 @@
 let products;
 
 //API REQUEST
-const fetchproducts = async() => {
+const fetchProducts = async() => {
     products = await fetch(
         'http://localhost:3000/api/products/').then(res =>res.json());
     
-        console.log(products);
+        
 };
 
-fetchproducts();
+const showProducts = async() => {
+    await fetchProducts();
+
+
+items.innerHTML = (
+    
+    products
+    .filter(product => product.name.toLowerCase())
+    .map(product => (
+        `
+        <article>
+        <img src="${product.imageUrl}" />
+        <h3 class="productName">${product.name}</h3>
+        <p class="productDescription">${product.description}</p>
+         </article>  
+
+        `
+    )).join('')
+);
+};
+showProducts();
