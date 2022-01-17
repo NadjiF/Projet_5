f719eef4bd69bf4645ca2fe06b38d1f794b97bd3
Nadji Fali <fali.nadji@hotmail.fr>
2022-01-17 16:20:24 +0100

    js script

--- front/js/script.js
+++ front/js/script.js
@@ -1,32 +1,15 @@
-let products;
+const header = documeny.getElementById("header");
+const  
 
 //API REQUEST
-const fetchProducts = async() => {
-    products = await fetch(
-        'http://localhost:3000/api/products/').then(res =>res.json());
-        console.log(products)
+function getProducts() {
+    fetch('http://localhost:3000/api/products/')
+    .then((res) => res.json())
+    .then ((products) => {
+        console.log(products);
+
+
     
         
-};
-
-const showProducts = async() => {
-    await fetchProducts();
-
-
-items.innerHTML = (
-    
-    products
-    .filter(product => product.name.toLowerCase())
-    .map(product => (
-        `
-        <article>
-        <img src="${product.imageUrl}" />
-        <h3 class="productName">${product.name}</h3>
-        <p class="productDescription">${product.description}</p>
-        </article>  
-
-        `
-    ))
-);
-};
-showProducts();
+    });
+}
