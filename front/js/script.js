dac03ca724d574a38e0f4272a0dbfe063c52ea8a
Nadji Fali <fali.nadji@hotmail.fr>
2022-01-18 16:16:47 +0100

    modif js index

--- front/js/script.js
+++ front/js/script.js
@@ -1,15 +1,39 @@
-const header = documeny.getElementById("header");
-const  
+let productsData = [];
 
-//API REQUEST
-function getProducts() {
-    fetch('http://localhost:3000/api/products/')
-    .then((res) => res.json())
-    .then ((products) => {
-        console.log(products);
-
-
+const fetchProducts = async () => { 
+await fetch("http://localhost:3000/api/products/")
+.then((res) => res.json())
+    .then((data) => productsData = data);
     
-        
-    });
-}
+    console.log(productsData);
+};
+
+const productsDisplay = async () => {    
+    await fetchProducts();
+
+
+  items.innerHTML = productsData
+        .map(
+            (product) =>
+            `
+            
+            <a href="./product.html?id=42"${product._id}>
+            <article class="card">
+            <img src="${product.imageUrl}" />
+            <h3 class="productName">${product.name}</h3>
+            <p class="productDescription">${product.description}</p>
+            </article> 
+            </a>
+            
+
+            
+            
+            `
+
+
+        )
+        .join('');
+
+
+};
+productsDisplay();
