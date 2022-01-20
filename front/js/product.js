971d65a2c7eafdd39b9ec3fbf36f2e5cc38588cd
Nadji Fali <fali.nadji@hotmail.fr>
2022-01-20 17:37:53 +0100

    modif js product

--- front/js/product.js
+++ front/js/product.js
@@ -1,10 +1,37 @@
-const getProduct = async () => {
-  return fetch("http://localhost:3000/api/products")
-    .then((res) => res.json())
-    .then((data) => getOneProduct = data);
+//object request by Id
+async function fetchById() {
+  try {
+    const queryStringUrlId = window.location.search;
+    //recupération de la chaine de requête dans l'url
+    const urlParams = new URLSearchParams(queryStringUrlId);
+    const productId = urlParams.get("id"); //recuperer id
 
-};
-console.log(getProduct)
-let imgProduct = "imgUrl";
+    const response = await fetch(
+      `http://localhost:3000/api/products/${productId}`
+    );
+    const idProduct = await response.json();
+    return idProduct;
+  } catch (error) {
+    console.log("error");
+  }
+}
+//displayproduct
+fetchById().then((idProduct) => {
+  idProduct;
+  displayProduct(idProduct);
+});
+//afficher les caractérisqtiques du produit dans le dom
+function displayProduct(idProduct) {
+  
+  let titlePage = document.querySelector("title");
+  titlePage.innerHTML = "Kanap -  " + idProduct.name;
+  let imgProd = document.querySelector(".item__img img");
+  imgProd = idProduct.imageUrl;
+  imgProd = idProduct.altTxt;
 
-console.log(imgProduct);
+  document.getElementById("title").textContent = idProduct.name;
+  document.getElementById("price").textContent = idProduct.price;
+  document.getElementById("description").textContent = idProduct.description;
+ console.log(idProduct);
+}
+
