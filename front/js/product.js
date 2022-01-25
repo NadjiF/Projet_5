f5bba5e897c1c8125aafa190359e2fe284b81723
Nadji Fali <fali.nadji@hotmail.fr>
2022-01-25 23:53:07 +0100

    modif product js

--- front/js/product.js
+++ front/js/product.js
@@ -8,28 +8,61 @@ async function fetchById() {
 
     const response = await fetch(
       `http://localhost:3000/api/products/${productId}`);
-    const idProduct = await response.json();
-    return idProduct;
+    const product = await response.json();
+    return product;
   } catch (error) {
     console.log("error");
   }
 }
 //displayproduct
-fetchById().then(idProduct => {
-  idProduct;
-  displayProduct(idProduct);
+fetchById().then(product => {
+  product;
+  displayProduct(product);
 });
 //afficher les caractérisqtiques du produit dans le dom
-function displayProduct(idProduct) {
+function displayProduct(product) {
   
   let titlePage = document.querySelector("title");
-  titlePage.innerHTML = "Kanap -  " + idProduct.name;
+  titlePage.innerHTML = "Kanap -  " + product.name;
   
-  let image = document.querySelector(".imageUrl");
-  image.src = idProduct.imageUrl;
+  
+  let images = document.querySelector('.imageUrl');  
+  images.src = product.imageUrl;
+  images.alt = product.altText; 
+  
+  document.getElementById("title").textContent = product.name;
 
-  document.getElementById("title").textContent = idProduct.name;
-  document.getElementById("price").textContent = idProduct.price;
-  document.getElementById("description").textContent = idProduct.description;
+  document.getElementById("price").textContent = product.price;
+
+  document.getElementById("description").textContent = product.description;
   
+  const selectElt = document.querySelector('select');
+    let optionElt;
+    for (color of product.colors) {
+        optionElt = document.createElement('option');
+        optionElt.setAttribute('value', color);
+        optionElt.textContent = color;
+        selectElt.appendChild(optionElt);
+    }
 }
+
+///////
+
+const sendToCart = document.getElementById('addToCart');
+
+sendToCart.addEventListener('click',(event) => {
+  event.preventDefault();
+
+const itemCart = {
+
+  id: product._id,
+  name: product.name,
+  quantity: quantity.value,
+  color: document.getElementById('colors').value,
+  price: product.price, 
+  img: product.imageUrl,
+  alt: product.altTxt,
+  }
+  console.log(itemCart);
+})
+
