66847dacc2498308d90d0f07369463533f6a4899
Nadji Fali <fali.nadji@hotmail.fr>
2022-03-09 11:22:43 +0100

    cart js

--- front/js/cart.js
+++ front/js/cart.js
@@ -209,3 +209,185 @@ if (location.href.search("confirmation") > 0) {
   }
   }
 }
+/////////////////////////////
+ //------Formulaire utilisateur
+
+    // On récupère nos balises d'input du formulaire
+    inputFirstName = document.querySelectorAll(
+        ".cart__order__form__question input"
+      )[0];
+      inputLastName = document.querySelectorAll(
+        ".cart__order__form__question input"
+      )[1];
+      inputAddress = document.querySelectorAll(
+        ".cart__order__form__question input"
+      )[2];
+      inputCity = document.querySelectorAll(
+        ".cart__order__form__question input"
+      )[3];
+      inputEmail = document.querySelectorAll(
+        ".cart__order__form__question input"
+      )[4];
+      // On récupère aussi les balise qui afficheront les erreurs si il y en a
+      errFirstName = document.querySelectorAll(
+        ".cart__order__form__question p"
+      )[0];
+      errLastName = document.querySelectorAll(
+        ".cart__order__form__question p"
+      )[1];
+      errAddress = document.querySelectorAll(".cart__order__form__question p")[2];
+      errCity = document.querySelectorAll(".cart__order__form__question p")[3];
+      errEmail = document.querySelectorAll(".cart__order__form__question p")[4];
+      // On récupère le bouton de soummision du formulaire
+      submitInfo = document.getElementById("order");
+  
+      validForm = false;
+      // EventListener qui récupère le prénom
+      inputFirstName.addEventListener("change", (e) => {
+        validFirstName(e.target.value);
+        contact.firstName = e.target.value;
+      });
+      // EventListener qui récupère le nom de famille
+      inputLastName.addEventListener("change", (e) => {
+        validLastName(e.target.value);
+        contact.lastName = e.target.value;
+      });
+      // EventListener qui récupère l'adresse
+      inputAddress.addEventListener("change", (e) => {
+        validAddress(e.target.value);
+        contact.address = e.target.value;
+      });
+      // EventListener qui récupère la ville
+      inputCity.addEventListener("change", (e) => {
+        validCity(e.target.value);
+        contact.city = e.target.value;
+      });
+      // EventListener qui récupère l'email
+      inputEmail.addEventListener("change", (e) => {
+        validEmail(e.target.value);
+        contact.email = e.target.value;
+      });
+      // Fonction qui vérifie à l'aide d'une RegExp que le champ prénom ne contiens pas de chiffre
+      function validFirstName(firstName) {
+        if (firstName.length == 0) {
+          errFirstName.innerHTML = "Votre prénom n'est pas renseigné !";
+          validForm = false;
+        } else if (!/[0-9]/.test(firstName)) {
+          errFirstName.innerText = "";
+          validForm = true;
+        } else {
+          errFirstName.innerText = "Votre prénom ne peut pas contenir de chiffre";
+          validForm = false;
+        }
+      }
+      // Fonction qui vérifie à l'aide d'une RegExp que le champ nom ne contiens pas de chiffre
+      function validLastName(lastName) {
+        if (lastName.length == 0) {
+          errLastName.innerHTML = "Votre prénom n'est pas renseigné !";
+          validForm = false;
+        } else if (!/[0-9]/.test(lastName)) {
+          errLastName.innerText = "";
+          validForm = true;
+        } else {
+          errLastName.innerText = "Votre nom ne peut pas contenir de chiffre";
+          return false;
+        }
+      }
+      function validAddress(address) {
+        if (address.length == 0) {
+          errAddress.innerHTML = "Votre adresse n'est pas renseigné !";
+          validForm = false;
+        } else {
+          errAddress.innerHTML = "";
+          validForm = true;
+        }
+      }
+      function validCity(city) {
+        if (city.length == 0) {
+          errCity.innerHTML = "Votre ville n'est pas renseigné !";
+          validForm = false;
+        } else {
+          errCity.innerHTML = "";
+          validForm = true;
+        }
+      }
+      // Fonction qui vérifie à l'aide d'une RegExp que le champ email est au format "texte@texte.txt"
+      function validEmail(email) {
+        let emailRegExp = new RegExp(
+          /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+        );
+        if (email.length == 0) {
+          errEmail.innerHTML = "Votre mail n'est pas renseigné !";
+          validForm = false;
+        } else if (emailRegExp.test(email)) {
+          errEmail.innerHTML = "";
+          validForm = true;
+        } else {
+          errEmail.innerHTML = "Votre mail n'est pas valide !";
+          validForm = false;
+        }
+      }
+  
+      // Eventlistener qui fonctionne seulement si tout les champs sont correctement rempli
+      submitInfo.addEventListener("click", (e) => {
+        e.preventDefault();
+        // Fonction fetch qui envoie à l'API un objet contenant l'objet 'contact' et le tableau 'products'
+        async function sendData() {
+          await fetch("https://api-kanap-eu.herokuapp.com/api/products/order", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ contact, products }),
+          })
+            // Ensuite on stock la réponse de l'api (orderId)
+            .then(function (response) {
+              return response.json();
+            })
+            .then(function (data) {
+              orderId = data.orderId;
+            });
+          // SI on a bien obtenu un orderId en réponse on redirige notre utilisateur
+          if (orderId != undefined || orderId != "") {
+            location.href = "confirmation.html?" + orderId + "#orderId";
+          }
+        }
+        // Fontion qui envoie les id de tout les produits dans le tableau product
+        function collectDatas() {
+          for (let article of panier) {
+            products.push(article.id);
+          }
+        }
+        // SI tout les champs sont valides (validForm = true) on continue l'execution du code
+        if (validForm) {
+          if (panier) {
+            new Swal({
+              title: "Commande en cours...",
+              icon: "question",
+              iconColor: "yellow",
+              confirmButtonColor: "#2c3e50",
+              showConfirmButton: false,
+            });
+            collectDatas();
+            sendData();
+          } else {
+            new Swal({
+              title: "Votre panier est vide !",
+              icon: "error",
+              iconColor: "red",
+              confirmButtonColor: "#2c3e50",
+              timer: 3000,
+              showConfirmButton: false,
+            });
+          }
+        }
+        // SINON on reverifie les champs
+        else {
+          validFirstName(inputFirstName.value);
+          validLastName(inputLastName.value);
+          validAddress(inputAddress.value);
+          validCity(inputCity.value);
+          validEmail(inputEmail.value);
+        }
+      });
+    
