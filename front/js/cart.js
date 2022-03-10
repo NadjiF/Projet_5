45f877762ce01dfe8e0326a45db01ece8464c9bc
Nadji Fali <fali.nadji@hotmail.fr>
2022-03-10 16:48:26 +0100

    modif js cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -23,7 +23,7 @@ function addArticle() {
         const contentSetting = document.createElement("div");
         const quantityProduct = document.createElement("div");
         const pQuantity = document.createElement("p");
-        const itemInput = document.createElement("input");
+        const itemQuantity = document.createElement("input");
         const settingDelete = document.createElement("div");
         const deleteItem = document.createElement("p")
 
@@ -34,13 +34,13 @@ function addArticle() {
         img.classList.add("cart__item__img");
         itemContent.classList.add("cart__item__content");
         contentDescription.classList.add("cart__item__description");
-        nameProduct.classList.add("cart__item__settings");
+        contentSetting.classList.add("cart__item__settings");
         quantityProduct.classList.add("cart__item__content__settings__quantity");
-        itemInput.classList.add("itemQuantity");
-        itemInput.setAttribute("type","number");
-        itemInput.setAttribute("name","itemQuantity");
-        itemInput.setAttribute("min","1");
-        itemInput.setAttribute("value",localItems[j].quantity);
+        itemQuantity.classList.add("itemQuantity");
+        itemQuantity.setAttribute("type","number");
+        itemQuantity.setAttribute("name","itemQuantity");
+        itemQuantity.setAttribute("min","1");
+        itemQuantity.setAttribute("value",localItems[j].quantity);
         settingDelete.classList.add("cart__item__content__settings__delete");
         deleteItem.classList.add("deleteItem");
 
@@ -51,15 +51,27 @@ function addArticle() {
         divImg.appendChild(img);
         divImg.querySelector("img").src = localItems[j].img;
         divImg.querySelector("img").alt = localItems[j].alt;
-        itemContent.appendChild(contentDescription) + itemContent.appendChild(nameProduct);
-        quantityProduct.appendChild(itemInput);
-        quantityProduct.appendChild(colorProduct) + quantityProduct.appendChild(itemInput);
-        quantityProduct.querySelector('p').textContent = 'quantité : ';
-        contentDescription.appendChild(nameProduct) + contentDescription.appendChild(priceProduct);
-        contentDescription.querySelector('h2').textContent = localItems[j].name + "-" + localItems[j].color;
-        let totalPriceUni = localItems[j].quantity*localItems[j].price;
-        contentDescription.querySelector('p').textContent = 'montant total produit : ' + totalPriceUni + ' € ' + ' - ' + '(Montant unitaire : ' + localItems[j].price + ' € )' ;
-        settingDelete.appendChild(deleteItem)
+
+        itemContent.appendChild(contentDescription) 
+        contentDescription.appendChild(nameProduct);
+        contentDescription.appendChild(colorProduct);
+        contentDescription.appendChild(priceProduct);
+        contentDescription.querySelector('h2').textContent = localItems[j].name;
+        contentDescription.querySelector('p').textContent = localItems [j].colorProduct;
+        
+        contentDescription.querySelector('p').textContent = localItems[j].priceProduct;
+
+        contentSetting.appendChild(quantityProduct);
+        quantityProduct.appendChild(pQuantity);
+        pQuantity.textContent = 'Qté : ';
+        itemQuantity.classList.add("itemQuantity");
+        itemQuantity.setAttribute("name", "itemQuantity");
+        itemQuantity.setAttribute("type", "number");
+        itemQuantity.setAttribute("min", "1");
+        itemQuantity.setAttribute("max", "100");
+        itemQuantity.setAttribute("value", article.quantity);
+       
+        settingDelete.appendChild(deleteItem);
         deleteItem.textContent = 'supprimer';
 
         itemsBalise.appendChild(article);
@@ -69,49 +81,3 @@ function addArticle() {
 addArticle();
 // affichage de la quantité et Prix total
 
-//////////////////////////////////////////////////////
-
-
-let vignettes = document.querySelectorAll(".cart__item");
-let suppressions = document.querySelectorAll(".deleteItem");
-let quantity = document.querySelectorAll(".itemQuantity");
-
-for (let i = 0; i < vignettes.length; i++) {
-  const qty = quantity[i];
-  const objet = panier[i];
-  qty.addEventListener("change", (e) => {
-    //On envoie la quantité selectionnée dans le panier
-    objet.quantity = parseInt(e.target.value);
-    // // On met à jour le localstorage
-    localStorage.setItem("panier", JSON.stringify(panier));
-    // on lance la fonction qui va mettre à jour le prix et le total de la page panier
-    recalc();
-  });
-}
-
-for (let i = 0; i < vignettes.length; i++) {
-  const suppr = suppressions[i];
-  let colorId = panier[i].color;
-  let dataId = panier[i].id;
-  // let article = panier[i];
-  suppr.addEventListener("click", () => {
-    // On supprime de notre panier l'élément de la boucle selectionné via splice()
-    let filtre = panier.filter(function (article) {
-      return article.id != dataId || article.color != colorId;
-    });
-
-    panier = filtre;
-    console.log(panier);
-
-    // on supprime le code HTML de ce même élément
-    document
-      .querySelector(
-        `[data-id='${dataId}']` && `[data-color='${colorId}']`
-      )
-      .remove();
-    // On met à jour le localstorage
-    localStorage.setItem("panier", JSON.stringify(panier));
-    // on lance la fonction qui va mettre à jour le prix et le total de la page panier
-    recalc();
-  });
-}
